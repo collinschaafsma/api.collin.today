df43e6343d434bb2f2bee2d48f351be546c19c32
Collin Schaafsma <collin@collin.io>
2020-04-08 22:02:39 -0600

    adding activities entity and migration

--- src/activities/activity.entity.ts
+++ src/activities/activity.entity.ts
@@ -1,10 +1,63 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import {
+  IsUUID,
+  IsOptional,
+  IsNotEmpty,
+  IsString,
+  IsDateString,
+  MinLength,
+} from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  Index,
+} from 'typeorm';
+import { CrudValidationGroups } from '@nestjsx/crud';
+import { UUID } from '../types/global';
+
+const { UPDATE } = CrudValidationGroups;
 
 @ObjectType()
 export class Activity {
-  @Field(type => ID)
-  id: string;
+  @IsUUID()
+  @Field(/* istanbul ignore next */ (_type) => ID)
+  @PrimaryGeneratedColumn('uuid')
+  @IsNotEmpty()
+  public id!: UUID;
+
+  @IsOptional({ groups: [UPDATE] })
+  @MinLength(1, { groups: [UPDATE] })
+  @Index()
+  @Column('varchar')
+  @Field()
+  @IsNotEmpty()
+  @IsString()
+  public title!: string;
+
+  @IsOptional({ groups: [UPDATE] })
+  @MinLength(1, { groups: [UPDATE] })
+  @Index()
+  @Column('varchar')
+  @Field()
+  @IsString()
+  public description!: string;
 
   @Field()
-  title: string;
+  @IsNotEmpty()
+  @IsDateString()
+  public publishAt!: Date;
+
+  @Field()
+  @CreateDateColumn()
+  @IsNotEmpty()
+  @IsDateString()
+  public createdAt!: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  @IsNotEmpty()
+  @IsDateString()
+  public updatedAt!: Date;
 }
