79245e270ea59a21649dc77172c3df435753c7f0
Collin Schaafsma <collin@collin.io>
2020-04-09 21:42:55 -0600

    eslint disable no inferrable types

--- src/activities/dto/activities.args.ts
+++ src/activities/dto/activities.args.ts
@@ -1,14 +1,14 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
-import { Max, Min } from 'class-validator';
+import { Min, Max, IsInt, IsNotEmpty } from 'class-validator';
+
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 
 @ArgsType()
 export class ActivitiesArgs {
-  @Field(type => Int)
-  @Min(0)
-  skip: number = 0;
-
-  @Field(type => Int)
+  @Field(/* istanbul ignore next */ (_type) => Int)
   @Min(1)
   @Max(50)
-  take: number = 25;
+  @IsInt()
+  @IsNotEmpty()
+  limit: number = 25;
 }
