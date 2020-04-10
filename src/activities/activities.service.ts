650fae98b3f48d9b10f456a3c0a3dc2e42854151
Collin Schaafsma <collin@collin.io>
2020-04-09 22:02:37 -0600

    service wip

--- src/activities/activities.service.ts
+++ src/activities/activities.service.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { NewActivityInput } from './dto/new-activity.input';
 import { ActivitiesArgs } from './dto/activities.args';
 import { Activity } from './activity.entity';
 
 @Injectable()
 export class ActivitiesService {
+  constructor(
+    @InjectRepository(Activity)
+    private activitiesRepository: Repository<Activity>,
+  ) {}
 
   async create(data: NewActivityInput): Promise<Activity> {
     return {} as any;
   }
 
   async findOneById(id: string): Promise<Activity> {
-    return {} as any;
+    return await this.activitiesRepository.findOne(id);
   }
 
   async findAll(activitiesArgs: ActivitiesArgs): Promise<Activity[]> {
-    return [] as Activity[];
+    return await this.activitiesRepository.find();
   }
 
-  async remove(id: string): Promise<boolean> {
-    return true;
+  async remove(id: string): Promise<void> {
+    await this.activitiesRepository.delete(id);
   }
 }
